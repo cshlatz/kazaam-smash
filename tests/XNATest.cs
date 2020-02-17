6d955a2415ba82ea1b6d62e6a8fcbadb5d0ff118
Connor Shlatz <cshlatz@gmail.com>
2020-02-17 18:55:44 -0500

    Initial commit with basic XNA test

---
+++ tests/XNATest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using Kazaam;
+
+namespace kazaamSmash {
+    [TestFixture()]
+    public class XNATest {
+        [Test()]
+        public void XNAGameTest() {
+            XNAGame Game = new XNAGame();
+            Assert.IsInstanceOf<XNAGame>(Game);
+        }
+    }
+}
